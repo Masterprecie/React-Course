126b51e5e6922f3cd3d9c82c5a6ff2d3713dc406
Precious <ikpaprecious2@gmail.com>
2022-10-13 17:19:41 +0100

    usestate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Boot from './Components/Boot';
 import Login from './Components/Login';
 import UseStateCounter from './Components/useState/UseStateCounter';
 import {Routes, Route} from 'react-router-dom'
+import ReactProject1 from './Components/Project1/ReactProject1';
 
 
 
@@ -11,7 +12,8 @@ function App() {
     <Routes>
       <Route path={'/'} element= {<Boot/>} />
       <Route path={'/Login'} element= {<Login/>}/>
-      <Route path={'/UseStateCounter'} element= {<UseStateCounter/>} />
+      <Route path={'/UseStateCounter'} element={<UseStateCounter />} />
+      <Route path={'/ReactProject1'} element = {<ReactProject1/>}/>
     </Routes>
   );
 }
---
+++ src/Components/Project1/ReactProject1.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import '../Project1/Style.css';
+
+import { data1 } from '../Project1/data1';
+
+const ReactProject1 = () => {
+
+  const [members, setMembers] = useState(data1)
+
+  const removeItem = (id) => {
+    let newMembers = members.filter((person) => person.id !== id);
+    setMembers(newMembers);
+  };
+  return (
+    <>
+      <main>
+        <section className='container'>
+          <h1 className='heading'>
+            5 birthdays today"
+          </h1>
+          <article className='person'>
+       {
+          members.map((person) => {
+            const { id, name, age, img } = person;
+            return (
+              <div key={id} className= 'div'>              
+                  <img src={{img}} alt = '' />
+                  <h1>{name}</h1>
+                  <h2>{age}</h2>  
+                  {<button className='btn' onClick={() => removeItem(id)}>Remove Item</button> }
+              </div>
+          )
+        })
+      }
+            <button style={{ marginTop: '10px'}} onClick={() => setMembers([])}>Clear Members</button>
+      </article>
+        </section>
+      </main>
+     
+
+        
+    </>
+  )
+}
+
+export default ReactProject1
